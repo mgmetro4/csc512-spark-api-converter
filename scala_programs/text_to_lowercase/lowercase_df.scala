dc42507d9eb3af2c67041f0b68d68d44158f7646
mgmetro <mgmetro@ncsu.edu>
2018-11-06 17:54:23 -0500

    upload lowercase tests

--- scala_programs/text_to_lowercase/lowercase_df.scala
+++ scala_programs/text_to_lowercase/lowercase_df.scala
@@ -1,8 +1,10 @@
- /* RDD
+ /* Dataframe
   * Convert text from file to lowercase. 
   * .collect() splits lines into arrays.
   * you can view original by calling .collect() on the sc directly */
 
-sc.textFile(“textfile.txt”)
-    .map(line => line.toLowerCase)
+spark.read.textFile(“textfile.txt”)
+    .selectExpr("") 
     .collect()
+
+    //no idea how to grab lines from Dataframe
--- scala_programs/text_to_lowercase/lowercase_ds.scala
+++ scala_programs/text_to_lowercase/lowercase_ds.scala
@@ -1,8 +1,8 @@
- /* RDD
+ /* Dataset
   * Convert text from file to lowercase. 
   * .collect() splits lines into arrays.
   * you can view original by calling .collect() on the sc directly */
 
-sc.textFile(“textfile.txt”)
+spark.read.textFile(“textfile.txt”)
     .map(line => line.toLowerCase)
     .collect()
