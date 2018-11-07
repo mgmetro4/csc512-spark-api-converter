0d194b3dafb2c0078420ac67400e91755d1d1758
mgmetro <mgmetro@ncsu.edu>
2018-11-06 19:03:37 -0500

    uploding sorting, rdd works only

--- scala_programs/sort_LtG/sort_LtG.ds.scala
+++ scala_programs/sort_LtG/sort_LtG.ds.scala
@@ -1,11 +1,12 @@
-/* RDD
+/* Dataset
  * Sort random text file of numbers from least to greatest 
  * echo "10, 1, 50, 4, 18, 35" > numbers.txt */
 
-sc.textFile(“numbers.txt”)
-	.map(line => line.split(",")
-	.map(elem => elem.trim)
-    .sortBy(_.toInt))
+spark.textFile(“numbers.txt”)
+    .map(line => line.split(",")
+    .map(elem => elem.trim)
+    // below does not work. Not sure why
+    .map(row=>((_.toInt)(row), row)).orderBy("_1").map(_._2))
     .collect() //creates another array, but don't know how else to view answers on terminal window easily
 
 
