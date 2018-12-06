de9507358d6d906ba59d7587027eafaf9e309ad7
mgmetro <mgmetro@ncsu.edu>
2018-12-06 16:21:09 -0500

    this might work better....

--- part3/last_day/function.scala
+++ part3/last_day/function.scala
@@ -1,10 +1,11 @@
-def day(date: String) = {
-  var ret = 0
+def last_day(date: String) = {
+  var ret = ""
   var count = 0
   var month = 0
   var year = 0
   var char = date.substring(0, 1)
 
+ 
  while( char != "-") {
     count +=1
     char = date.substring(count, count+1)
@@ -15,10 +16,9 @@ def day(date: String) = {
 
   if(month < 8) {
     if(month%2 != 0) {
-      ret = 31
+      ret = "31"
     }
     else if (month == 2) {
-      println("FEB")
       count = 0
       char = date.substring(0,1)
        while( char != "-") {
@@ -27,25 +27,25 @@ def day(date: String) = {
         }
         year = (date.substring(count-4, count)).toInt
       if((year % 4 == 0) && !( (year%100 == 0) && (year%400 != 0) )  )  {
-        ret = 29
+        ret = "29"
       }
       else {
-        ret = 28
+        ret = "28"
       }
     }
     else {
-      ret = 30
+      ret = "30"
     }
 
   }
   
   else {
     if (month%2 == 0) {
-      ret = 31
+      ret = "31"
     }
     else {
-      ret = 30
+      ret = "30"
     }
   }
-  ret
+  var answer = date.substring(0,8) + ret
 }
