1b90b7e3d47d109755ec9b540a734a0eb696d119
mgmetro <mgmetro@ncsu.edu>
2018-12-09 03:40:56 -0500

    fixing pad on lpad

--- part3/lpad/function.scala
+++ part3/lpad/function.scala
@@ -1,25 +1,27 @@
 def lpad(str: String, len: Int, pad: String) = {
   
   var ret = ""
+  var padding = ""
   var i = str.size
   var diff = 0
 
   if( len <= str.size ) {
-    ret = str.substring(0, len-1)
+    ret = str.substring(0, len)
   } 
   else {
     ret += str
     while( i < len) {
         diff = len - i
         if(diff < pad.size) {
-          ret = pad.substring(0, diff) + ret
-          i = ret.size
+          padding += pad.substring(0, diff)
+          i = padding.size + ret.size
         }
         else {
-          ret = pad + ret
-          i = ret.size
+          padding += pad
+          i = ret.size + padding.size
         }
     }
+    ret = padding + ret
   }
-  ret
+  ret 
 }
