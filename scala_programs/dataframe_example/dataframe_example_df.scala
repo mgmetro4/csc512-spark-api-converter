a993b299664659574748fb8c83a750bc208d93f2
mgmetro <mgmetro@ncsu.edu>
2018-12-09 09:16:40 -0500

    fixing dataframe_example windows to unix problem

--- scala_programs/dataframe_example/dataframe_example_df.scala
+++ scala_programs/dataframe_example/dataframe_example_df.scala
@@ -1,6 +1,6 @@
 //Dataframe
 
-spark.range(10, 24).selectExpr(“id as _1”)
-	.selectExpr(“_1 as _1”, “if (_1%3==0), _1*10, _1*2) as _2”)
-	.selectExpr(“_1+_2 as _1”)
+spark.range(10, 24).selectExpr("id as _1")
+	.selectExpr("_1 as _1", "if (_1%3==0), _1*10, _1*2) as _2")
+	.selectExpr("_1+_2 as _1")
 	.collect()
--- scala_programs/dataframe_example/dataframe_example_ds.scala
+++ scala_programs/dataframe_example/dataframe_example_ds.scala
@@ -1,6 +1,6 @@
 //Dataset
 
 spark.range(10, 24)
-	.map( i => {val j=i%3; (I, if (j==0) i*10 else i*2) })
+	.map( i => {val j=i%3; (i, if (j==0) i*10 else i*2) })
 	.map(r => r._1+r._2)
 	.collect()
