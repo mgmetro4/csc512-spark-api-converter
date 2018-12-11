314943e7ee135b950f5e8fe9c56dd6e91e944932
mgmetro <mgmetro@ncsu.edu>
2018-12-11 17:14:52 -0500

    fixed dataset errors

--- part0/sort_LtG/sort_LtG_ds.scala
+++ part0/sort_LtG/sort_LtG_ds.scala
@@ -1,5 +1,5 @@
 //Dataset
 
 spark.range(0,10)
-    .map(row => (( ((a:Long) => Math.sin(a))) (row),row)).orderBy("_1").map(_._2)
+    .map(row => ( ((a:Long) => Math.sin(a)) (row),row)).orderBy("_1").map(_._2)
     .collect()
